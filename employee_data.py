4129af982a2929a1967dc785bf624c14f3636baa
cherukuSujithmani <cherukusujithmani@gmail.com>
2026-01-15 00:00:51 +0530

    first commit

---
+++ employee_data.py
@@ -0,0 +1,11 @@
+import math
+employee_id=201211101015
+name='Sujith mani cheruku'
+hourly_rate = 25.2
+is_active = True
+
+print(employee_id,name,hourly_rate,is_active)
+print(f"Employe name is {name} and his employee_id is {employee_id} and his hourly salery is {hourly_rate} and is he avalible now: {is_active}")
+print(int(hourly_rate))
+print(round(hourly_rate))
+print(math.ceil(hourly_rate))
